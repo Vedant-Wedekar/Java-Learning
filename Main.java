8e39090d04e9f7a46122c07b8978c85125f18605
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-07-12 03:18:55 +0530

    Add Main class with a simple Java program

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        int age = 20 ;
+        String name = "Vedant Wedekar";
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("Let's write some code!");
+        System.out.println("Your age is: " + age);
+        System.out.println("Your name is: " + name);
+    }
+}
