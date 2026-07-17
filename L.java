5792304826e0751632997de630e96ca4f61d2299
Vedant-Wedekar <vedantwedekar@gmail.com>
2026-07-17 12:13:39 +0530

    Add L class with a simple Java program for learning purposes

---
+++ L.java
@@ -0,0 +1,8 @@
+public class L{
+
+    public static void main(String[] args){
+        System.out.println("hii my name is  Ved   ");
+        System.out.println("im learning  java with the purpose of  solving dsa  questions  using  it  ");
+        System.out.println(" and here is again im starting  to learn java and it is my Day 1   ");
+    }
+}
